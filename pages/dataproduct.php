c38b6c0c5efe15f4d09aca7946039f86061560a2
ErlanNoviar <noviarerlan13@gmail.com>
2023-10-03 12:18:17 +0700

    first commit

---
+++ pages/dataproduct.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Gallery</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+    <!-- Ekko Lightbox -->
+    <link rel="stylesheet" href="../plugins/ekko-lightbox/ekko-lightbox.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="../index.php" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+
+            <!-- Right navbar links -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Navbar Search -->
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                        <i class="fas fa-search"></i>
+                    </a>
+                    <div class="navbar-search-block">
+                        <form class="form-inline">
+                            <div class="input-group input-group-sm">
+                                <input class="form-control form-control-navbar" type="search" placeholder="Search"
+                                    aria-label="Search">
+                                <div class="input-group-append">
+                                    <button class="btn btn-navbar" type="submit">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                    <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                                        <i class="fas fa-times"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                        <i class="fas fa-expand-arrows-alt"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+                        <i class="fas fa-th-large"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+            <!-- Brand Logo -->
+            <a href="../index.php" class="brand-link">
+                <img src="../dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                    style="opacity: .8">
+                <span class="brand-text font-weight-light">Welcome</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="../dist/img/logo.png" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">Erlan Noviar</a>
+                    </div>
+                </div>
+
+                <!-- SidebarSearch Form -->
+                <div class="form-inline">
+                    <div class="input-group" data-widget="sidebar-search">
+                        <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                            aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-sidebar">
+                                <i class="fas fa-search fa-fw"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+                        <li class="nav-item menu-open">
+                            <a href="../index.php" class="nav-link">
+                                <i class="nav-icon fas fa-home"></i>
+                                <p>
+                                    Dashboard
+                                    <!-- <i class="right fas fa-angle-left"></i> -->
+                                </p>
+                            </a>
+                        </li>
+                        <!-- my-product -->
+                        <li class="nav-item menu-open">
+                            <a href="newproduct.php" class="nav-link active">
+                                <i class="nav-icon 	fas fa-shopping-cart"></i>
+                                <p>
+                                    Product
+                                </p>
+                            </a>
+                        </li>
+                        <!-- Akhir my-product -->
+                        <!-- Login-layout -->
+                        <li class="nav-item menu-open">
+                            <a href="login-layout.php" class="nav-link">
+                                <i class="nav-icon fas fa-sign-in-alt"></i>
+                                <p>
+                                    Login
+                                </p>
+                            </a>
+                        </li>
+                        <!-- Akhir Login-layout -->
+                        <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1>Product</h1>
+                        </div>
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active">Product</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card card-primary">
+                                <div class="card-header">
+                                    <h4 class="card-title">List Product</h4>
+                                </div>
+                                <!-- product -->
+                                <div class="card-body">
+                                    <div class="card-deck">
+                                        <?php
+                                        // include data dari listing_product.php
+                                        require 'listing_product.php';
+
+                                        // looping untuk menampilkan product
+                                        foreach ($ListProduct1 as $item) {
+                                        ?>
+                                        <div class="card">
+                                            <img src="<?php echo $item['image']; ?>" class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title"><?php echo $item['title']; ?></h5>
+                                                <p class="card-text"><?php echo $item['description']; ?></p>
+                                                <p><?php echo $item['prince']; ?></p>
+                                                <a href="#" class="btn btn-primary">BUY</a>
+                                            </div>
+                                        </div>
+                                        <?php
+                                        }
+                                        ?>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                <div class="card-deck">
+                                        <?php
+                                        // include data dari listing_product.php
+                                        require 'listing_product.php';
+
+                                        // looping untuk menampilkan product
+                                        foreach ($ListProduct2 as $item) {
+                                        ?>
+                                        <div class="card">
+                                            <img src="<?php echo $item['img']; ?>" class="card-img-top" alt="...">
+                                            <div class="card-body">
+                                                <h5 class="card-title"><?php echo $item['title']; ?></h5>
+                                                <p class="card-text"><?php echo $item['description']; ?></p>
+                                                <p><?php echo $item['prince']; ?></p>
+                                                <a href="#" class="btn btn-primary">BUY</a>
+                                            </div>
+                                        </div>
+                                        <?php
+                                        }
+                                        ?>
+                                    </div>
+                                </div>
+                                <!-- akhir Product -->
+                            </div>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <footer class="main-footer">
+            <strong>Copyright &copy; 2023 <a href="https://github.com/ErlanNoviar">Erlan Noviar</a>.</strong>
+            <div class="float-right d-none d-sm-inline-block">
+                SIB - PT. Arkatama Multi Solusindo
+            </div>
+        </footer>
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
+
+    <!-- jQuery -->
+    <script src="../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Ekko Lightbox -->
+    <script src="../plugins/ekko-lightbox/ekko-lightbox.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../dist/js/adminlte.min.js"></script>
+    <!-- Filterizr-->
+    <script src="../plugins/filterizr/jquery.filterizr.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../dist/js/demo.js"></script>
+    <!-- Page specific script -->
+    <script>
+        $(function () {
+            $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+                event.preventDefault();
+                $(this).ekkoLightbox({
+                    alwaysShowClose: true
+                });
+            });
+
+            $('.filter-container').filterizr({ gutterPixels: 3 });
+            $('.btn[data-filter]').on('click', function () {
+                $('.btn[data-filter]').removeClass('active');
+                $(this).addClass('active');
+            });
+        })
+    </script>
+</body>
+
+</html>
---
+++ pages/listing_product.php
@@ -0,0 +1,16 @@
+<?php
+$ListProduct1 = array(
+    array("image" => "../dist/img/product/Hoodie.jpg", "title" => "Jaket Zipper Pria Terkini", "description" => "Jaket Pantai Motif Blue Sea Onesize", "prince" => "Rp. 52.000"),
+    array("image" => "../dist/img/product/J.jpg", "title" => "Jaket Parasut Pria", "description" => "Bahan Despo lembut, Tersedia ukuran M, L & XL", "prince" => "Rp. 66.900"),
+    array("image" => "../dist/img/product/Baseball.jpg", "title" => "Jaket Baseball Pria Terkini", "description" => "Tersedia ukuran M, L & XL", "prince" => "Rp. 70.000"),
+    array("image" => "../dist/img/product/jenas.jpg", "title" => "Jaket Pria Model Jeans", "description" => "Cocok untuk hunting. <br> Tersedia ukuran M, L & XL", "prince" => "Rp. 119.000"),
+);
+
+$ListProduct2 = array(
+    array("img" => "../dist/img/product/sepatu.jpg", "title" => "SEPATU SANTAI PRIA ARDIVO", "description" => "SNEAKERS - KASUAL TERBARU 2023", "prince" => "Rp. 80.000"),
+    array("img" => "../dist/img/product/S.Putih.jpg", "title" => "Sepatu Pria Running", "description" => "Sneakers Model Terbaru 2021", "prince" => "Rp. 1200.000"),
+    array("img" => "../dist/img/product/PARAYU.jpg", "title" => "Parayu Sepatu Sneakers Kets Pria", "description" => "Hight Elve Sport", "prince" => "Rp. 150.000"),
+    array("img" => "../dist/img/product/Converse.jpg", "title" => "Converse 70s Ox Black White gret Premium Quality", "description" => "Made In Vietnam", "prince" => "Rp. 160.000"),
+);
+
+?>
